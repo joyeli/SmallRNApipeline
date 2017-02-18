e6acb160a8803f80cb2ec75ca28beb47c36bb955
john <CHChang810716@gmail.com>
2017-02-18 21:57:48 +0800

    remove bad inlcude

--- include/link.sh
+++
@@ -1,8 +0,0 @@
-ln -sf ../src/ AGO
-ln -sf /home/joyel/CCD/include/CCD CCD
-ln -sf /home/joyel/CPT/include/CPT CPT
-ln -sf /home/joyel/CCD/include/pokemon/ pokemon
-ln -sf /home/john/slam_quant_seq/include/std_addon/ std_addon
-ln -sf /home/joyel/CCD/include/parallel_pool parallel_pool
-# ln -sf /home/joyel/GitDoc/Tailor/src/ Tailor
-ln -sf /home/joyel/CPT/include/debug_utility debug_utility
