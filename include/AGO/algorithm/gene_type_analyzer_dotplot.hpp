8d4bade2da782f853b1873ffd1bdfc06bf45f8ae
Joye Li <joyeli@outlook.com>
2018-07-26 13:50:00 +0800

    fix href of dotplot

--- include/AGO/algorithm/gene_type_analyzer_dotplot.hpp
+++ include/AGO/algorithm/gene_type_analyzer_dotplot.hpp
@@ -810,7 +810,7 @@ class GeneTypeAnalyzerDotplot
         if( !isSeed && biotype != "BioType/" )
         {
             output << "                    .append('a')" << "\n";
-        	output << "                    .attr('xlink:href', function(d){ mirAnno = d.miRNA.split( '_' )[0]; return '../SqAlign/index.php?TSV_File=$TSV_File.tsv&Annotation_Select=' + " << ( biotype == "miRNA" || biotype == "mirtron" || biotype == "miRNA_mirtron" ? "mirAnno.substring( 0, mirAnno.length -3 )" : "mirAnno" ) << "; })" << "\n";
+        	output << "                    .attr('xlink:href', function(d){ mirAnno = d.miRNA.split( '_' )[0]; return '../SqAlign/index.php?TSV_File=$TSV_File1.tsv&Annotation_Select=' + " << ( biotype == "miRNA" || biotype == "mirtron" || biotype == "miRNA_mirtron" ? "mirAnno.substring( 0, mirAnno.length -3 )" : "mirAnno" ) << "; })" << "\n";
             output << "                    .attr('target', '_blank')" << "\n";
         }
 
